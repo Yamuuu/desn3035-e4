95bc32e6dff7b195a29fbad63d71bec7c84692e4
Yamuuu <72154426+Yamuuu@users.noreply.github.com>
2024-10-08 17:49:35 +0000

    Second commit, 3page with Next.js

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,18 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Nav() {
     return <>
             <div className="col-3">
                 <div className="list-group">
-                    <a href="home.html" className="list-group-item list-group-item-action">
+                    <Link href="/" className="list-group-item list-group-item-action">
                         Home
-                    </a>
-                    <a href="about.html" className="list-group-item list-group-item-action">
+                    </Link>
+                    <Link href="/about" className="list-group-item list-group-item-action">
                         About Us
-                    </a>
-                    <a href="contact.html" className="list-group-item list-group-item-action">
+                    </Link>
+                    <Link href="/contact" className="list-group-item list-group-item-action">
                         Contact Us
-                    </a>
+                    </Link>
                 </div>
                 <p className="small text-danger text-center mt-3 text-warning">
                     Coffee: the magic potion that turns morning mayhem into midday clarity.
---
+++ app/contact/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Nav from "../components/nav";
+
+export default function Page() {
+    return <>
+    <div className="container py-4">
+        <div className="row">
+
+            <header />
+
+            <Nav />
+
+            <div className="col-9">
+                <img className="d-block w-100 mb-3" src="https://fastly.picsum.photos/id/251/1280/720.jpg?hmac=YDyCcHsQDDzvzCfZ-MLi2fr2pFqpoH9OqSqmJFBwV7w" />
+                <h1>Welcome</h1>
+                <p>
+                    Reach us at 647-xxxxx
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+    </>
+  }
